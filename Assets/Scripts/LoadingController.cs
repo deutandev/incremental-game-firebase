fbedd479b1bec7561a64497c3bc836cfad5b0da5
Andika Fathin Azfar <deutandev@pm.me>
2021-10-02 13:10:17 +0700

    Add loading scene

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingController : MonoBehaviour
+{
+    private void Start ()
+    {
+        UserDataManager.Load ();
+        SceneManager.LoadScene (1);
+    }
+}
