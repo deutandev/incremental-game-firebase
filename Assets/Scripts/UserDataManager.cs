1337eca1fcdcfbb39ab63e307fc043fa8e392aa4
Andika Fathin Azfar <deutandev@pm.me>
2021-10-02 13:04:37 +0700

    Add scripts to save/load user data

---
+++ Assets/Scripts/UserDataManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UserDataManager
+{
+    private const string PROGRESS_KEY = "Progress";
+    public static UserProgressData Progress;
+
+    public static void Load()
+    {
+        // Cek apakah ada data yang tersimpan sebagai PROGRESS_KEY
+        if (!PlayerPrefs.HasKey(PROGRESS_KEY))
+        {
+            // Jika tidak ada, maka buat data baru
+            Progress = new UserProgressData();
+            Save();
+        }
+        else
+        {
+            // Jika ada, maka timpa progress dengan yang sebelumnya
+            string json = PlayerPrefs.GetString(PROGRESS_KEY);
+            Progress = JsonUtility.FromJson<UserProgressData>(json);
+        }
+    }
+
+    public static void Save()
+    {
+        string json = JsonUtility.ToJson(Progress);
+        PlayerPrefs.SetString(PROGRESS_KEY, json);
+    }
+}
